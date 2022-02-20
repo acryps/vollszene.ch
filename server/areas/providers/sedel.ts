c12514293f102aa8462ae0704d58340f105eb1ee
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-02-20 15:43:56 +0100

    add sedel

---
+++ server/areas/providers/sedel.ts
@@ -0,0 +1,38 @@
+import { Provider } from "../provider";
+import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
+import { Event } from "../../managed/database";
+
+
+export default class SedelProvider extends Provider {
+    name = 'sedel';
+
+    async fetch() {
+        const events = [];
+
+        const html = await fetch('https://sedel.ch').then(res => res.text());
+        const page = new JSDOM(html);
+
+        let topDate = new Date(new Date().toDateString());
+        topDate.setUTCHours(0);
+
+        for (let eventElement of page.window.document.querySelectorAll('.month-list li')) {
+            const event = new Event();
+            
+            const dateComponents = eventElement.querySelector('time').attributes['datetime'].value.split(/-|T/g);
+            event.date = new Date(Date.UTC(+dateComponents[0], +dateComponents[1] - 1, +dateComponents[2]));
+
+            const link = eventElement.querySelector('a');
+
+            event.name = link.textContent;
+            event.link = `https://sedel.ch${link.href}`;
+
+            // skip events that are today, we should already have them in the list!
+            if (!event.name.includes('ABGESAGT')) {
+                events.push(event);
+            }
+        }
+        
+        return events;
+    }
+}
