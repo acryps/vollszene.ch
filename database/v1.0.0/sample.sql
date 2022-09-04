de56aa5e015deabd3793cc6e312f390c697a99d7
Eggsecuter <adrian.habeler@gmail.com>
2022-09-04 16:26:28 +0200

    fix db 001 sample script

--- database/v1.0.0/sample.sql
+++ database/v1.0.0/sample.sql
@@ -1,4 +1,4 @@
 INSERT INTO event (date, name, link, hash, image_url, ticket_link, ticket_available, ticket_price, highlight, host_id)
-VALUES (NOW(), 'lorem', 'ipsum', 'lorem', 'ipsum', 'lorem', true, 'ipsum', false, (
+VALUES (NOW(), 'lorem', 'ipsum', 'lorem', 'ipsum', 'lorem', true, 0, false, (
     SELECT id FROM host WHERE name = 'Nordstern'
 ));
